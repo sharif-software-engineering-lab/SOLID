b2eb816eb21295405925397b701819138b36f417
Parsa Mohammadian <parsa2820@gmail.com>
2023-11-06 16:26:50 +0330

    Use new methods in Main class
    - Fix single responsibility principle

--- Step_3_With_SOLID/src/Main.java
+++ Step_3_With_SOLID/src/Main.java
@@ -11,29 +11,33 @@ public class Main {
         OrderService orderService = null;
         String customerName;
         Order order;
-        int customerAnswerForOrder=0;
-        int customerAnswerForPaymentMethod=0;
 
         System.out.println("Enter Customer Name : ");
         customerName = scanner.nextLine();
         order = new Order(customerName);
 
-        //Step 1 : Select Order Items
-        while (customerAnswerForOrder!=3){
-            System.out.println("For Ordering Sandwich enter 1.");
-            System.out.println("For Ordering Pizza enter 2.");
-            System.out.println("For submit your order enter 3");
-            customerAnswerForOrder = scanner.nextInt();
+        getOrder(scanner, order);
 
-            if(customerAnswerForOrder==1){
-                order.addItem(new Food("sandwich",1000));
-            } else if(customerAnswerForOrder==2){
-                order.addItem(new Food("pizza",2000));
-            }
+        orderService = getOrderService(scanner, orderService, customerName);
 
+        payPrice(orderService, order);
 
+        //Finally Print Bill
+        System.out.println(order);
+    }
+
+    private static void payPrice(OrderService orderService, Order order) {
+        //Step3 : pay price
+        System.out.println("Pay Price:");
+        if(orderService instanceof OnlineOrderService){
+            orderService.onlineOrderPayment(order.getTotalPrice());
+        } else if(orderService instanceof OnSiteOrderService){
+            orderService.onSiteOrderPayment(order.getTotalPrice());
         }
+    }
 
+    private static OrderService getOrderService(Scanner scanner, OrderService orderService, String customerName) {
+        int customerAnswerForPaymentMethod;
         //Step2 : Select Payment Method
         System.out.println("Enter Your Payment Method (1 for online and 2 for on-site):");
         customerAnswerForPaymentMethod = scanner.nextInt();
@@ -44,19 +48,24 @@ public class Main {
             orderService = new OnSiteOrderService();
             orderService.onSiteOrderRegister(customerName);
         }
+        return orderService;
+    }
 
-        //Step3 : pay price
-        System.out.println("Pay Price:");
-        if(orderService instanceof OnlineOrderService){
-            orderService.onlineOrderPayment(order.getTotalPrice());
-        } else if(orderService instanceof OnSiteOrderService){
-            orderService.onSiteOrderPayment(order.getTotalPrice());
+    private static void getOrder(Scanner scanner, Order order) {
+        int customerAnswerForOrder=0;
+        //Step 1 : Select Order Items
+        while (customerAnswerForOrder !=3){
+            System.out.println("For Ordering Sandwich enter 1.");
+            System.out.println("For Ordering Pizza enter 2.");
+            System.out.println("For submit your order enter 3");
+            customerAnswerForOrder = scanner.nextInt();
+
+            if(customerAnswerForOrder ==1){
+                order.addItem(new Food("sandwich",1000));
+            } else if(customerAnswerForOrder ==2){
+                order.addItem(new Food("pizza",2000));
+            }
         }
-
-        //Finally Print Bill
-        System.out.println(order);
-
-
     }
 
 }
